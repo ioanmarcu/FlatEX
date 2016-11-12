3f501c38639a9dea1a9538ef7e034d20ab5cdc30
Marcu Ioan <ionelmarcu0@gmail.com>
2016-11-12 18:42:08 +0200

    first commit

---
+++ src/main/java/com/ionelmarcu/flatex/FlatExApplication.java
@@ -0,0 +1,12 @@
+package com.ionelmarcu.flatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatExApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ionelmarcu/flatex/FlatExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ionelmarcu.flatex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlatExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
